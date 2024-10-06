ff981f01b97973ed6f7c58b1318e57a454b4f067
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-10-06 11:05:05 -0400

    added example file for db schema #2

--- forms/yearly/form_ip_110.py
+++ forms/yearly/form_ip_110.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
-from ..dao.db import DAO
-import polars as pl
+from typing import Optional
+from sqlmodel import Field, Session, SQLModel, create_engine
 
-class IP110Validator(BaseModel):
+class IP110Yearly(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     company_name: str
     address: str
     email: str
@@ -73,90 +73,83 @@ class IP110Validator(BaseModel):
     signature: str
     rank: str
 
-class IP_110FormView():
-    def __init__(self, form: IP110Validator):
-        self.raw = form
-        self.df = []
-
-    def incert_to_db(self):
-        for key, value in self.raw:
-            self.df.append(pl.Series(key, [value]))
-
-        df = pl.DataFrame(self.df)
-        DAO().insert_forms(df, "IP_110", 15)
 
 if __name__ == "__main__":
-    external_data = {
-        "company_name": "John Doe1",
-        "address": "123 Main Street",
-        "email": "name@test.com",
-        "liaison_officer": "John Doe2",
-        "ssn": "123456789",
-        "tel": "123456789",
-        "fax": "123456789",
-        "legal_form": "John Doe3",
-        "cfc": "John Doe4",
-        "business_type": "John Doe5",
-        "business_function": "John Doe6",
-        "branches": "John Doe7",
-        "closing_date": "John Doe8",
-        "start_year": 2021,
-        "end_year": 2023,
-        "incomes_services_revenues_1": 1.0,
-        "incomes_services_revenues_2": 1.0,
-        "incomes_industries_1": 1.0,
-        "incomes_industries_2": 1.0,
-        "incomes_persons_1": 1.0,
-        "incomes_persons_2": 1.0,
-        "incomes_sale_merchandise_1": 1.0,
-        "incomes_sale_merchandise_2": 1.0,
-        "incomes_rents_1": 1.0,
-        "incomes_rents_2": 1.0,
-        "incomes_interests_1": 1.0,
-        "incomes_interests_2": 1.0,
-        "incomes_capital_gain_loss_1": 1.0,
-        "incomes_capital_gain_loss_2": 1.0,
-        "others_incomes_1": 1.0,
-        "others_incomes_2": 1.0,
-        "total_income_1": 1.0,
-        "total_income_2": 1.0,
-        "expenses_1": 1.0,
-        "expenses_2": 1.0,
-        "expenses_salaries_wages_bonus_1": 1.0,
-        "expenses_salaries_wages_bonus_2": 1.0,
-        "expenses_interests_1": 1.0,
-        "expenses_interests_2": 1.0,
-        "expenses_rents_1": 1.0,
-        "expenses_rents_2": 1.0,
-        "expenses_depreciation_1": 1.0,
-        "expenses_depreciation_2": 1.0,
-        "expenses_bad_debts_1": 1.0,
-        "expenses_bad_debts_2": 1.0,
-        "expenses_donation_1": 1.0,
-        "expenses_donation_2": 1.0,
-        "expenses_sales_tax_1": 1.0,
-        "expenses_sales_tax_2": 1.0,
-        "expenses_machinery_1": 1.0,
-        "expenses_machinery_2": 1.0,
-        "other_purchases_1": 1.0,
-        "other_purchases_2": 1.0,
-        "licenses_1": 1.0,
-        "licenses_2": 1.0,
-        "other_expenses_1": 1.0,
-        "other_expenses_2": 1.0,
-        "total_expenses_1": 1.0,
-        "total_expenses_2": 1.0,
-        "net_profit_1": 1.0,
-        "net_profit_2": 1.0,
-        "net_profit_income_tax_1": 1.0,
-        "net_profit_income_tax_2": 1.0,
-        "profit_after_tax_1": 1.0,
-        "profit_after_tax_2": 1.0,
-        "withheld_tax_1": 1.0,
-        "withheld_tax_2": 1.0,
-        "signature": "John Doe11",
-        "rank": "John Doe12",
-    }
-    data = IP110Validator(**external_data)
-    form = IP_110FormView(data)
-    form.incert_to_db()
+    engine = create_engine("sqlite:///db.sqlite")
+    SQLModel.metadata.create_all(engine)
+
+    form = IP110Yearly(
+        company_name="John Doe1",
+        address="123 Main Street",
+        email="name@test.com",
+        liaison_officer="John Doe2",
+        ssn="123456789",
+        tel="123456789",
+        fax="123456789",
+        legal_form="John Doe3",
+        cfc="John Doe4",
+        business_type="John Doe5",
+        business_function="John Doe6",
+        branches="John Doe7",
+        closing_date="John Doe8",
+        start_year=2021,
+        end_year=2023,
+        incomes_services_revenues_1=1.0,
+        incomes_services_revenues_2=1.0,
+        incomes_industries_1=1.0,
+        incomes_industries_2=1.0,
+        incomes_persons_1=1.0,
+        incomes_persons_2=1.0,
+        incomes_sale_merchandise_1=1.0,
+        incomes_sale_merchandise_2=1.0,
+        incomes_rents_1=1.0,
+        incomes_rents_2=1.0,
+        incomes_interests_1=1.0,
+        incomes_interests_2=1.0,
+        incomes_capital_gain_loss_1=1.0,
+        incomes_capital_gain_loss_2=1.0,
+        others_incomes_1=1.0,
+        others_incomes_2=1.0,
+        total_income_1=1.0,
+        total_income_2=1.0,
+        expenses_1=1.0,
+        expenses_2=1.0,
+        expenses_salaries_wages_bonus_1=1.0,
+        expenses_salaries_wages_bonus_2=1.0,
+        expenses_interests_1=1.0,
+        expenses_interests_2=1.0,
+        expenses_rents_1=1.0,
+        expenses_rents_2=1.0,
+        expenses_depreciation_1=1.0,
+        expenses_depreciation_2=1.0,
+        expenses_bad_debts_1=1.0,
+        expenses_bad_debts_2=1.0,
+        expenses_donation_1=1.0,
+        expenses_donation_2=1.0,
+        expenses_sales_tax_1=1.0,
+        expenses_sales_tax_2=1.0,
+        expenses_machinery_1=1.0,
+        expenses_machinery_2=1.0,
+        other_purchases_1=1.0,
+        other_purchases_2=1.0,
+        licenses_1=1.0,
+        licenses_2=1.0,
+        other_expenses_1=1.0,
+        other_expenses_2=1.0,
+        total_expenses_1=1.0,
+        total_expenses_2=1.0,
+        net_profit_1=1.0,
+        net_profit_2=1.0,
+        net_profit_income_tax_1=1.0,
+        net_profit_income_tax_2=1.0,
+        profit_after_tax_1=1.0,
+        profit_after_tax_2=1.0,
+        withheld_tax_1=1.0,
+        withheld_tax_2=1.0,
+        signature="John Doe11",
+        rank="John Doe12",
+    )
+
+    with Session(engine) as session:
+        session.add(form)
+        session.commit()
