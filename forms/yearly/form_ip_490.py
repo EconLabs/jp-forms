f38919e8ef1c36f938e2637dfccfdbf01820b2fb
eduardobonilla189 <eduardo.bonilla1@upr.edu>
2024-10-15 13:39:05 -0400

    Correction form_ip_490.py #1

--- forms/yearly/form_ip_490.py
+++ forms/yearly/form_ip_490.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel 
-from ..dao.db import DAO
-import polars as pl
+from sqlmodel import Field, Session, SQLModel, create_engine
+from typing import Optional
 
-class IP490Validator(BaseModel):
+class IP490Validator(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     company_name: str
     address: str
     email: str
@@ -72,15 +72,4 @@ class IP490Validator(BaseModel):
     withheld_tax_2: float
     signature: str
     rank: str
-    
-class IP_490FormView():
-    def __init__(self, form: IP490Validator):
-        self.raw = form
-        self.df = []
-        
-    def incert_to_db(self):
-        for key, value in self.raw:
-            self.df.append(pl.Series(key, [value]))
-            
-        df = pl.DataFrame(self.df)
-        DAO().insert_forms(df, "IP_490", 15)
+
