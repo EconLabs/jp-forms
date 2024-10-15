d365c5786ff4921f944a4957d93b991e8b64025a
eduardobonilla189 <eduardo.bonilla1@upr.edu>
2024-10-15 14:02:14 -0400

    Correction form_ip_230.py #1

--- forms/yearly/form_ip_230.py
+++ forms/yearly/form_ip_230.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
-from ..dao.db import DAO
-import polars as pl
+from sqlmodel import Field, Session, SQLModel, create_engine
+from typing import Optional
 
-class IP230Validator(BaseModel):
+class IP230Validator(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     company_name: str
     address: str
     email: str
@@ -73,14 +73,3 @@ class IP230Validator(BaseModel):
     signature: str
     rank: str
     
-class IP_230FormView():
-    def __init__(self, form: IP230Validator):
-        self.raw = form
-        self.df = []
-        
-    def incert_to_db(self):
-        for key, value in self.raw:
-            self.df.append(pl.Series(key, [value]))
-            
-        df = pl.DataFrame(self.df)
-        DAO().insert_forms(df, "IP_230", 15)
