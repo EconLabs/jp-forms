4dd3f58dd872361a18b68bf787a54f1945b14994
eduardobonilla189 <eduardo.bonilla1@upr.edu>
2024-11-01 13:56:26 -0400

    Correction form_jp_383.py #1

--- forms/yearly/form_jp_383.py
+++ forms/yearly/form_jp_383.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field, SQLModel
 from typing import Optional
 
-class IP383Validator(SQLModel, table=True):
+class JP383Validator(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     company_name: str
     address: str
