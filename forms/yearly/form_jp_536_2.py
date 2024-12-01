d404299f783250b17125616256ec3c105ef730d6
eduardobonilla189 <eduardo.bonilla1@upr.edu>
2024-12-01 17:49:52 -0400

    1 migrate forms (#3)
    * remove whitespace #1
    
    * finish form_ip_210.py #1
    
    * Finish form_ip_210.py #1
    
    * Finish form_ip_210.py #1
    
    * Finish form_ip_220.py #1
    
    * Correction form_ip_210.py
    
    * correction to form IP220 #1
    
    * Finish form_ip_230.py #1
    
    * Finish form_ip_310.py #1
    
    * Finish form_ip_310b.py  #1
    
    * Finish form_ip_420.py #1
    
    * Finish form_ip_440.py #1
    
    * Finish form_ip_440g.py #1
    
    * Finish form_ip_480.py #1
    
    * Finish form_ip_480a.py #1
    
    * Finish form_ip_490.py #1
    
    * Finish form_ip_510.py #1
    
    * Finish form_ip_520.py #1
    
    * Correction form_ip_520.py #1
    
    * Correction form_ip_510.py #1
    
    * Correction form_ip_510.py #1
    
    * Correction form_ip_490.py #1
    
    * correction form_ip_480a.py #1
    
    * Correction form_ip_480.py #1
    
    * Correction form_ip_440g.py #1
    
    * Correction form_ip_440.py #1
    
    * Correction form_ip_420.py #1
    
    * Correction form_ip_310b.py #!
    
    * Correction form_ip_310.py #1
    
    * Correction form_ip_230.py #1
    
    * Correction form_ip_220.py #1
    
    * Correction form_ip_210.py #1
    
    * Correction form_ip_110.py #1
    
    * Correction form_ip_520a.py #1
    
    * Finish form_ip_520s.py #1
    
    * Finish form_ip_530.py #1
    
    * Finish form_ip_540.py #1
    
    * Finish form_ip_540a.py #1
    
    * Finish form_ip_540j.py #1
    
    * Finish form_ip_540p.py
    
    * Finish form_ip_540s.py #1
    
    * Finish form_ip_540v.py #1
    
    * Finish form_ip_610.py #1
    
    * Finish form_ip_620.py #1
    
    * Finish form_ip_710.py #1
    
    * Correction form_ip_710.py #1
    
    * Correction form_ip_620.py #1
    
    * Correction form_ip_610.py #1
    
    * Correction form_ip_540v.py #1
    
    * Correction form_ip_540s.py #1
    
    * Correction form_ip_540p.py #1
    
    * Correction form_ip_540j.py
    
    * Correction form_ip_540a.py #1
    
    * Correction form_ip_540.py #1
    
    * Correction form_ip_530.py #1
    
    * Correction form_ip_520s.py #1
    
    * Correction form_ip_520.py #1
    
    * Correction form_ip_510.py #1
    
    * Correction form_ip_490.py #1
    
    * Correction form_ip_480a.py #1
    
    * Correction form_ip_480.py #1
    
    * Correction form_ip_440g.py #1
    
    * Correction form_ip_440.py #1
    
    * Correction form_ip_420.py #1
    
    * Correction form_ip_310b.py #1
    
    * Correction form_ip_310.py #1
    
    * Correction form_ip_230.py #1
    
    * Correction form_ip_220.py #1
    
    * Correction form_ip_210.py #1
    
    * Correction form_ip_110.py #1
    
    * Correction form_ip_520a.py #1
    
    * Finish form_ip_720.py #1
    
    * Finish form_ip_810.py
    
    * Finish form_jp_304.py #1
    
    * Finish form_jp_361.py #1
    
    * Finish form_jp_362.py #1
    
    * Finish form_jp_363.py #1
    
    * Finish form_jp_364.py #1
    
    * Finish form_jp_375.py #1
    
    * Finish form_jp_383.py #1
    
    * Finish form_jp_529.py #1
    
    * Finish form_jp_536_2.py #1
    
    * Finish form_jp_541.py #1
    
    * Finish form_jp_544_2.py #1
    
    * Correction form_jp_304.py #1
    
    * Correction form_jp_361.py #1
    
    * Correction form_jp_362.py #1
    
    * Correction form_jp_363.py #1
    
    * Correction form_jp_364.py #1
    
    * Correction form_jp_375.py #1
    
    * Correction form_jp_383.py #1
    
    * Correction form_jp_529.py #1
    
    * Correction form_jp_536_2.py #1
    
    * Correction form_jp_541.py #1
    
    * Correction form_jp_544_2.py #1
    
    * Finish form_jp_544.py #1
    
    * Finish form_jp_547.py #1
    
    * Finish form_jp_560_2.py #1
    
    * Finish form_jp_560_63110.py #1
    
    * Finish form_jp_560_63111.py #1
    
    * Finish form_jp_560_63210.py #1

--- forms/yearly/form_jp_536_2.py
+++ forms/yearly/form_jp_536_2.py
@@ -1,62 +1,75 @@
-from django.shortcuts import render
-from src.dao.data_db_dao import DAO
-import polars as pl
+from sqlmodel import Field, SQLModel
+from typing import Optional
 
-
-def JP_536_2(request):
-    if request.method == "POST":
-        # Retrieve form data
-        start_year = request.POST.get("start_year")
-        end_year = request.POST.get("end_year")
-        inventario1 = request.POST.get("inventario1")
-        inventario2 = request.POST.get("inventario2")
-        compras1 = request.POST.get("compras1")
-        compras2 = request.POST.get("compras2")
-        depre1 = request.POST.get("depre1")
-        depre2 = request.POST.get("depre2")
-        maquinaria1 = request.POST.get("maquinaria1")
-        maquinaria2 = request.POST.get("maquinaria2")
-        equipo1 = request.POST.get("equipo1")
-        equipo2 = request.POST.get("equipo2")
-        computadora1 = request.POST.get("computadora1")
-        computadora2 = request.POST.get("computadora2")
-        alquiler1 = request.POST.get("alquiler1")
-        alquiler2 = request.POST.get("alquiler2")
-        licencia1 = request.POST.get("licencia1")
-        licencia2 = request.POST.get("licencia2")
-        company_name = request.POST.get("company_name")
-        phone = request.POST.get("phone")
-        name_title = request.POST.get("name_title")
-        date = request.POST.get("date")
-
-        data = [
-            pl.Series("start_year", [start_year], dtype=pl.String),
-            pl.Series("end_year", [end_year], dtype=pl.String),
-            pl.Series("inventario1", [float(inventario1)], dtype=pl.Float64),
-            pl.Series("inventario2", [float(inventario2)], dtype=pl.Float64),
-            pl.Series("compras1", [float(compras1)], dtype=pl.Float64),
-            pl.Series("compras2", [float(compras2)], dtype=pl.Float64),
-            pl.Series("depre1", [float(depre1)], dtype=pl.Float64),
-            pl.Series("depre2", [float(depre2)], dtype=pl.Float64),
-            pl.Series("maquinaria1", [float(maquinaria1)], dtype=pl.Float64),
-            pl.Series("maquinaria2", [float(maquinaria2)], dtype=pl.Float64),
-            pl.Series("equipo1", [float(equipo1)], dtype=pl.Float64),
-            pl.Series("equipo2", [float(equipo2)], dtype=pl.Float64),
-            pl.Series("computadora1", [float(computadora1)], dtype=pl.Float64),
-            pl.Series("computadora2", [float(computadora2)], dtype=pl.Float64),
-            pl.Series("alquiler1", [float(alquiler1)], dtype=pl.Float64),
-            pl.Series("alquiler2", [float(alquiler2)], dtype=pl.Float64),
-            pl.Series("licencia1", [float(licencia1)], dtype=pl.Float64),
-            pl.Series("licencia2", [float(licencia2)], dtype=pl.Float64),
-            pl.Series("company_name", [company_name], dtype=pl.String),
-            pl.Series("phone", [phone], dtype=pl.String),
-            pl.Series("name_title", [name_title], dtype=pl.String),
-            pl.Series("date", [date], dtype=pl.String),
-        ]
-        
-        df = pl.DataFrame(data)
-        DAO().insert_forms(df, "JP_536_2", 18)
-
-        return render(request, "forms/succesfull.html")
-    return render(request, "forms/yearly/balanza_de_pagos/JP-536-2.html")
+class JP536_2Validator(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    company_name: str
+    address: str
+    email: str
+    liaison_officer: str
+    ssn: str
+    tel: str
+    fax: str
+    legal_form: str
+    cfc: str
+    business_type: str
+    business_function: str
+    branches: str
+    closing_date: str
+    start_year: int
+    end_year: int
+    incomes_services_revenues_1: float
+    incomes_services_revenues_2: float
+    incomes_industries_1: float
+    incomes_industries_2: float
+    incomes_persons_1: float
+    incomes_persons_2: float
+    incomes_sale_merchandise_1: float
+    incomes_sale_merchandise_2: float
+    incomes_rents_1: float
+    incomes_rents_2: float
+    incomes_interests_1: float
+    incomes_interests_2: float
+    incomes_capital_gain_loss_1: float
+    incomes_capital_gain_loss_2: float
+    others_incomes_1: float
+    others_incomes_2: float
+    total_income_1: float
+    total_income_2: float
+    expenses_1: float
+    expenses_2: float
+    expenses_salaries_wages_bonus_1: float
+    expenses_salaries_wages_bonus_2: float
+    expenses_interests_1: float
+    expenses_interests_2: float
+    expenses_rents_1: float
+    expenses_rents_2: float
+    expenses_depreciation_1: float
+    expenses_depreciation_2: float
+    expenses_bad_debts_1: float
+    expenses_bad_debts_2: float
+    expenses_donation_1: float
+    expenses_donation_2: float
+    expenses_sales_tax_1: float
+    expenses_sales_tax_2: float
+    expenses_machinery_1: float
+    expenses_machinery_2: float
+    other_purchases_1: float
+    other_purchases_2: float
+    licenses_1: float
+    licenses_2: float
+    other_expenses_1: float
+    other_expenses_2: float
+    total_expenses_1: float
+    total_expenses_2: float
+    net_profit_1: float
+    net_profit_2: float
+    net_profit_income_tax_1: float
+    net_profit_income_tax_2: float
+    profit_after_tax_1: float
+    profit_after_tax_2: float
+    withheld_tax_1: float
+    withheld_tax_2: float
+    signature: str
+    rank: str
 
