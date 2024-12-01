d957cd8e4c7435b51795bea5a516d4bc43b085c5
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2024-12-01 22:10:52 +0000

    4 toml (#5)
    * added nix file #4
    
    * added basic toml #4

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+
+  packages = [ pkgs.uv ];
+
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+  '';
+
+}
